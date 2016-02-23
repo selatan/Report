6277af9dada64de6a18cbe341e308e2e578a96d3
a1 <353658617@qq.com>
2016-02-23 17:25:05 +0800

    修改db数据

--- ReportEmail/Product_Main.py
+++ ReportEmail/Product_Main.py
@@ -2,8 +2,8 @@
 
 import sqlite3
 from ReportEmail.Product import Product
+
 # import sys
-#
 # reload(sys)
 # sys.setdefaultencoding('utf8')
 
@@ -12,17 +12,15 @@ connection = sqlite3.connect("/Users/a1/Desktop/ProjectReport/myData.db")
 
 
 dao = Product()
-id = dao.insert(connection,("bestie_Android","1.3.2",unicode("模块1,模块2")))
+id = dao.insert(connection,("bestie_Android","1.3.2",("模块1,模块2")))
 print(id)
-id = dao.insert(connection,("bestie_Android","1.3.3",unicode("模块1,模块2")))
+id = dao.insert(connection,("bestie_Android","1.3.3",("模块1,模块2")))
 print(id)
 
 #dao.insert(connection,("bestie_Android","1.4.0","模块1,模块2,模块3"))
 #dao.insert(connection,("bestie_Ios","1.1.0","模块1,模块4"))
 #dao.insert(connection,("bestie_IOS","1.2.0","模块1,模块2,模块3,模块4"))
 #dao.insert(connection,("bestie_IOS","1.3.1","模块2,模块3,模块4"))
-
-
 
 
 
