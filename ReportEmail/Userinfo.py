047d283d7173cb31de6b6cd8a1f6f5013be19aeb
a1 <353658617@qq.com>
2016-02-25 17:35:19 +0800

    userinfo中添加查询方法

--- ReportEmail/Userinfo.py
+++ ReportEmail/Userinfo.py
@@ -15,3 +15,10 @@ class Userinfo():
         cu=conn.cursor()
         cu.execute("UPDATE userinfo SET pw = ? and email_list = ? and bugzilla_pw = ? WHERE username = ?",param)
         conn.commit()
+
+     #查询数据
+    def select(self,conn,param):
+        cu = conn.cursor()
+        cu.execute("SELECT * FROM userinfo where username = ?",param)
+        #print(cu.fetchall())  #从结果中取出所有数据
+        return cu.fetchall()
