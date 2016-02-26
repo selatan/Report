c1d2104d039703ee398f5675fdd53b9c7c0e0569
a1 <353658617@qq.com>
2016-02-26 18:05:19 +0800

    添加getConnection方法

--- ReportEmail/Main.py
+++ ReportEmail/Main.py
@@ -1,42 +1,50 @@
 # coding=utf-8
+import sqlite3
 
-from Userinfo import *
-from Newrequest import *
-from Newdate import *
-from Product import *
-from Receiver import *
-from Returntesting import *
-from Userproduct import *
+# from ReportEmail.Userinfo import *
+# from ReportEmail.Newrequest import *
+# from ReportEmail.Newdate import *
+# from ReportEmail.Product import *
+# from ReportEmail.Receiver import *
+# from ReportEmail.Returntesting import *
+# from ReportEmail.Userproduct import *
+#
+# # 为每一个类创建一个对象
+# userinfo = Userinfo()
+# newrequest = Newrequest()
+# newdate = Newdate()
+# product = Product()
+# receiver = Receiver()
+# returntesting = Returntesting()
+# userproduct = Userproduct()
+#
+#
+# class DbManager:
+#     @classmethod
+#     def userinfo_insert(cls, conn, params):
+#         userinfo.insert(conn, params)
+#
+#     @classmethod
+#     def userinfo_select(cls, conn, params):
+#         result = userinfo.select(conn, params)
+#         return result
+#
+#     @classmethod
+#     def userinfo_update(cls, conn, params):
+#         userinfo.update(conn, params)
+#
+#     @classmethod
+#     def newrequest_insert(cls,conn,params):
+#         newrequest.insert(conn,params)
+#
+#     @classmethod
+#     def newrequest_remove(cls,conn,params):
+#         newrequest.remove(conn,params)
 
-# 为每一个类创建一个对象
-userinfo = Userinfo()
-newrequest = Newrequest()
-newdate = Newdate()
-product = Product()
-receiver = Receiver()
-returntesting = Returntesting()
-userproduct = Userproduct()
 
 
-class DbManager:
-    @classmethod
-    def userinfo_insert(cls, conn, params):
-        userinfo.insert(conn, params)
 
-    @classmethod
-    def userinfo_select(cls, conn, params):
-        result = userinfo.select(conn, params)
-        return result
-
-    @classmethod
-    def userinfo_update(cls, conn, params):
-        userinfo.update(conn, params)
-
-    @classmethod
-    def newrequest_insert(cls,conn,params):
-        newrequest.insert(conn,params)
-
-    @classmethod
-    def newrequest_remove(cls,conn,params):
-        newrequest.remove(conn,params)
-
+def get_connection():
+    conn = sqlite3.connect("/Users/a1/Desktop/ProjectReport/myData.db")
+    #conn = sqlite3.connect("E:/workspace/python/Report/db/myData.db")
+    return conn
